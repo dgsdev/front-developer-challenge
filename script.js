8bd06e9ad24a65f8e28aff8822b11d712d56a02d
dgsdev <dgspramos@gmail.com>
2021-04-11 13:57:53 -0300

    Day 2 Page E-mail

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Olá Linx, New Job o/!")
+//*** Consumo API Produtos ***
+
+const produtos = fetch("https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1")
+.then(response => response.json())
+.then(data => {
+    const ul = document.querySelector('[data-js="produtos"]')
+    ul.innerHTML = data.products[1].image
+
+   
+});
+

f6a481faf4f8a71907194cf4e78e41e410e22a86
dgsdev <dgspramos@gmail.com>
2021-04-11 21:52:43 -0300

    CSS Page E-mail, Fetch API and Button more products

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
 //*** Consumo API Produtos ***
 
-const produtos = fetch("https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1")
-.then(response => response.json())
-.then(data => {
-    const ul = document.querySelector('[data-js="produtos"]')
-    ul.innerHTML = data.products[1].image
+const url = `https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1`;
 
-   
-});
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    //console.log(data.products)
 
+    const lisProducts = data.products.reduce((accumulator, data) => {
+      accumulator += `
+        <li class="card-prod">
+        <img class-card-image" src="http:${data.image}" alt="${data.name}" />        
+        <h1 class="card-title">${data.name}</h1>
+        <p class="card-subtitle">${data.description}</p>
+        <strike class="card-strike">De: R$${data.oldPrice}</strike>
+        <h3 class="card-price">Por: R$${data.price}</h3>
+        <p class="card-parcel">ou ${data.installments.count}x de R$ ${data.installments.value}</p>
+        </li>`;
+      //console.log(data.installments)
+      return accumulator;
+    }, "");
+    
+    const ul = document.querySelector('[data-js="products"]')
+        ul.innerHTML = lisProducts
+
+    console.log(ul);
+    
+  });
+
+  function moreProducts() {
+      const urlNext = `https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=2`;
+      
+      fetch(urlNext)
+      .then((response) => response.json())
+      .then((data) => {    
+      
+        const lisProducts = data.products.reduce((accumulator, data) => {
+          accumulator += `
+            <li class="card-prod">
+            <img class-card-image" src="http:${data.image}" alt="${data.name}" />        
+            <h1 class="card-title">${data.name}</h1>
+            <p class="card-subtitle">${data.description}</p>
+            <strike class="card-strike">De: R$${data.oldPrice}</strike>
+            <h3 class="card-price">Por: R$${data.price}</h3>
+            <p class="card-parcel">ou ${data.installments.count}x de R$ ${data.installments.value}</p>
+            </li>`;
+          
+          return accumulator;
+        }, "");
+        
+        const ul = document.querySelector('[data-js="more-products"]')
+            ul.innerHTML = lisProducts    
+        
+      });
+    
+}
+  
+
+
+
+    

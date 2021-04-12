316f62d207aae2105f1e7ad26cfe958ada2c137d
dgsdev <dgspramos@gmail.com>
2021-04-12 18:37:40 -0300

    JS, Button more products and Styles CSS

---
+++ email.js
@@ -0,0 +1,41 @@
+const url = `https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=3`;
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    // console.log(data.products);
+
+    // for (let i = 0; i < data.products.length; i++) {
+    //   if (i == 2) break;
+    //   const lisProducts = data.products[1];
+
+      // `<li class="card-prod">
+      // <img class-card-image" src="http:${data.image}" alt="${data.name}" />
+      // <h1 class="card-title">${data.name}</h1>
+      // <p class="card-subtitle">${data.description}</p>
+      // <strike class="card-strike">De: R$${data.oldPrice}</strike>
+      // <h3 class="card-price">Por: R$${data.price}</h3>
+      // <p class="card-parcel">ou ${data.installments.count}x de R$ ${data.installments.value}</p>
+      // </li>`
+
+      const lisProducts = data.products.reduce((accumulator, data) => {
+        accumulator += `
+            <li class="card-prod">
+            <img class"card-image" src="http:${data.image}" alt="${data.name}" />        
+            <h1 class="card-title">${data.name}</h1>
+            <p class="card-subtitle">${data.description}</p>
+            <strike class="card-strike">De: R$${data.oldPrice}</strike>
+            <h3 class="card-price">Por: R$${data.price}</h3>
+            <p class="card-parcel">ou ${data.installments.count}x de R$ ${data.installments.value}</p>
+            <button class="card-btn">Comprar</button>
+            </li>`;
+
+        return accumulator;
+      }, "");
+
+      const ul = document.querySelector('[data-js="card-products"]');
+      ul.innerHTML = lisProducts;
+
+      console.log(lisProducts);
+    
+  });
--- script.js
+++ script.js
@@ -7,56 +7,49 @@ fetch(url)
   .then((data) => {
     //console.log(data.products)
 
-    const lisProducts = data.products.reduce((accumulator, data) => {
+    const liProducts = data.products.reduce((accumulator, data) => {
       accumulator += `
         <li class="card-prod">
-        <img class-card-image" src="http:${data.image}" alt="${data.name}" />        
+        <img class"card-image" src="http:${data.image}" alt="${data.name}" />        
         <h1 class="card-title">${data.name}</h1>
         <p class="card-subtitle">${data.description}</p>
         <strike class="card-strike">De: R$${data.oldPrice}</strike>
         <h3 class="card-price">Por: R$${data.price}</h3>
         <p class="card-parcel">ou ${data.installments.count}x de R$ ${data.installments.value}</p>
+        <button class="card-btn">Comprar</button>
         </li>`;
       //console.log(data.installments)
       return accumulator;
     }, "");
-    
-    const ul = document.querySelector('[data-js="products"]')
-        ul.innerHTML = lisProducts
 
-    console.log(ul);
-    
+    const ul = document.querySelector('[data-js="products"]');
+    ul.innerHTML = liProducts;
+
+    // console.log(ul);
   });
 
-  function moreProducts() {
-      const urlNext = `https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=2`;
-      
-      fetch(urlNext)
-      .then((response) => response.json())
-      .then((data) => {    
-      
-        const lisProducts = data.products.reduce((accumulator, data) => {
-          accumulator += `
+function moreProducts() {
+  const urlNext = `https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=2`;
+
+  fetch(urlNext)
+    .then((response) => response.json())
+    .then((data) => {
+      const lisProducts = data.products.reduce((accumulator, data) => {
+        accumulator += `
             <li class="card-prod">
-            <img class-card-image" src="http:${data.image}" alt="${data.name}" />        
+            <img class="card-image" src="http:${data.image}" alt="${data.name}" />        
             <h1 class="card-title">${data.name}</h1>
             <p class="card-subtitle">${data.description}</p>
             <strike class="card-strike">De: R$${data.oldPrice}</strike>
             <h3 class="card-price">Por: R$${data.price}</h3>
             <p class="card-parcel">ou ${data.installments.count}x de R$ ${data.installments.value}</p>
+            <button class="card-btn">Comprar</button>
             </li>`;
-          
-          return accumulator;
-        }, "");
-        
-        const ul = document.querySelector('[data-js="more-products"]')
-            ul.innerHTML = lisProducts    
-        
-      });
-    
+
+        return accumulator;
+      }, "");
+
+      const ul = document.querySelector('[data-js="more-products"]');
+      ul.innerHTML = lisProducts;
+    });
 }
-  
-
-
-
-    
